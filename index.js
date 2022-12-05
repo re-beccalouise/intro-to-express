617e1d1e3c4b691c1bf6d8028a9aee7328612602
re-beccalouise <rebeccamoorcroft@outlook.com>
2022-12-05 16:16:47 +0000

    added index.html

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 //needs to be initialised, its a function
+const path = require('path');
+//built into node js to avoid mac/windows/linux diff paths e.g. /\
 
 const app = express();
 
-//when app receives a GET request to the index page, do this...
-app.get('/', (req, res) => {
-    res.sendFile('hello world!');
+//when app (server) gets a GET request to the '/' do this...
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 
 app.listen(3000, () => {
     console.log('app listening on http://localhost:3000');
